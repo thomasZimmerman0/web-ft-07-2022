e85090c74057f31125438c5bacde8ec40d7c454c
Veronica Lino <Veronica@digitalcrafts.com>
2022-07-06 16:08:23 -0500

    lecture notes

--- lectures/week01/3-Wednesday/veros-class-notes-dont-touch/lecture.py
+++ lectures/week01/3-Wednesday/veros-class-notes-dont-touch/lecture.py
@@ -113,9 +113,104 @@
 
 # print(7 >= 9)
 
-a = not False 
-b  = True 
+# a = not False 
+# b  = True 
 
-c = a or b
+# c = a or b
 
-print(a)
+# print(a)
+
+# print(1)
+# print(2)
+
+# print('before')
+# age = 25
+# if (age == 26):  #True
+#     print(3)
+#     print(4)
+#     print(5)
+# elif( age== 25):
+#     print(26)
+# else: #default
+#     print(5.5)
+    
+# print('after')
+    
+# print(6)
+# print(7)
+
+# 1. add
+# 2. subtract
+# 3. multipy
+# 4. divide 
+
+# accept 2 numbers from our user
+
+
+# choice = int(input("""
+# 1. add
+# 2. subtract
+# 3. multipy
+# 4. divide              
+# """))
+
+# num1 = int(input('enter num 1 >>'))
+# num2 = int(input('enter num 2 >>'))
+
+# if choice == 1 :
+#     result = num1 + num2
+# elif choice == 2: 
+#     result = num1 - num2 
+# elif choice == 3: 
+#     result = num1 * num2 
+# elif choice == 4: 
+#     result = num1 / num2
+# else: 
+#     result = 'this is not a menu choice'
+    
+# print(result)
+
+
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+# print('hello world')
+
+# count = 0 
+
+
+print('before loop')
+
+while count < 10: # 11 < 10
+    print('hello world')
+    count = count + 1  # 3
+    # count *=1
+    
+print('outside loop')
+
+answer = ''
+
+while answer != 'when':  #WheN
+    answer = input('Say when: ')
+    answer = answer.lower() #when
+    
+print('Cheese')
+
+
+
