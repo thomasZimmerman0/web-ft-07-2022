f0014de8fb96541747021c439606bc3af0703baa
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-07-12 11:44:22 -0400

    Dictionary notes

---
+++ lectures/week02/2-Tuesday/MY-LectureNotes/scrapPaper.py
@@ -0,0 +1,18 @@
+
+
+
+
+my_dictionary = {}
+
+#? initalize 
+
+contacts = {
+    "firstName": "Shannon",
+    "phone": "333-333-3333",
+    "city": "Phoenix",
+    "zip": 77002,
+    "isFriendly": True,
+    0 : 12,
+    True: False,
+    "myList" : [0, 2, 4]
+}
