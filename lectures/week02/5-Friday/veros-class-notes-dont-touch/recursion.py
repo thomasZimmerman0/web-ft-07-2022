027b0bcd182647ac5be4dd6332a21c29c4105da7
Veronica Lino <Veronica@digitalcrafts.com>
2022-07-15 16:03:18 -0500

    recursion

---
+++ lectures/week02/5-Friday/veros-class-notes-dont-touch/recursion.py
@@ -0,0 +1,12 @@
+
+
+### 4 ^3 
+### 4 * 4 * 4 *  1 
+
+
+def power(base, exponent): # 4, 3
+    count = 1
+    solution = 1
+    while count < exponent: 
+        solution = solution * base  # 16 *4 = 64
+        count +=1
