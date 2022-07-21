15ee3032f5f60bea50856df5d53292e2926c3f43
Veronica Lino <Veronica@digitalcrafts.com>
2022-07-21 08:54:55 -0500

    daily problems

---
+++ lectures/week03/4-Thursday/MY-LectureNotes/daily_problem/color_data.py
@@ -0,0 +1,257 @@
+colors = [
+    {
+        "id": 4836,
+        "uid": "23bff2d5-2875-4e12-abfb-1eea88bd065e",
+        "hex_value": "#9ffa92",
+        "color_name": "sky",
+        "hsl_value": [
+            182,
+            0.11,
+            0.57
+            ],
+        "hsla_value": [
+            302,
+            0.24,
+            0.84,
+            0.2
+            ]
+    },
+    {
+        "id": 6718,
+        "uid": "037da165-c0e9-4115-a0fd-e2082e975b6c",
+        "hex_value": "#0f08f7",
+        "color_name": "amethyst",
+        "hsl_value": [
+            311,
+            0.02,
+            0.82
+            ],
+        "hsla_value": [
+            218,
+            0.51,
+            0.95,
+            0.6
+            ]
+    },
+    {
+        "id": 8185,
+        "uid": "c99490f8-2e78-48cf-85fb-ae3f4c32999e",
+        "hex_value": "#e56c03",
+        "color_name": "cyan",
+        "hsl_value": [
+            161,
+            0.7,
+            0.17
+            ],
+        "hsla_value": [
+            348,
+            0.49,
+            0,
+            0.4
+            ]
+    },
+    {
+        "id": 5898,
+        "uid": "a3177279-c196-4a79-9ceb-9dfe010d54bc",
+        "hex_value": "#ef9f74",
+        "color_name": "tan",
+        "hsl_value": [
+            334,
+            0.38,
+            0.76
+            ],
+        "hsla_value": [
+            317,
+            0.3,
+            0.34,
+            0.3
+            ]
+    },
+    {
+        "id": 1528,
+        "uid": "59d40df7-480c-4db8-a700-559097ca20bc",
+        "hex_value": "#2a86a8",
+        "color_name": "coral",
+        "hsl_value": [
+            149,
+            0.9,
+            0.09
+            ],
+        "hsla_value": [
+            251,
+            0.93,
+            0.64,
+            0.3
+            ]
+    },
+    {
+        "id": 5338,
+        "uid": "f4a37422-1c1d-4abd-9af0-d91d090da727",
+        "hex_value": "#3add12",
+        "color_name": "rose",
+        "hsl_value": [
+            287,
+            0.03,
+            0.29
+            ],
+        "hsla_value": [
+            283,
+            0.05,
+            0.42,
+            0.2
+            ]
+    },
+    {
+        "id": 966,
+        "uid": "446b74d0-a629-462c-bb9e-70ee7a874f98",
+        "hex_value": "#2e85bf",
+        "color_name": "rose",
+        "hsl_value": [
+            44,
+            0.79,
+            0.59
+            ],
+        "hsla_value": [
+            102,
+            0.48,
+            0.83,
+            0.9
+            ]
+    },
+    {
+        "id": 4427,
+        "uid": "08a9dca3-1043-405d-8352-5b89f0736991",
+        "hex_value": "#9715f1",
+        "color_name": "orange",
+        "hsl_value": [
+            343,
+            0.32,
+            0.27
+            ],
+        "hsla_value": [
+            82,
+            0.23,
+            0.96,
+            0.7
+            ]
+    },
+    {
+        "id": 8563,
+        "uid": "0d47eb1b-c293-46b1-8094-d29c893d4cb2",
+        "hex_value": "#ec2f69",
+        "color_name": "azure",
+        "hsl_value": [
+            136,
+            0.64,
+            0.86
+            ],
+        "hsla_value": [
+            332,
+            0.93,
+            0.46,
+            0
+            ]
+    },
+    {
+        "id": 7985,
+        "uid": "7b2e6285-785d-4ed0-997f-34122d7163bf",
+        "hex_value": "#e11103",
+        "color_name": "grey",
+        "hsl_value": [
+            221,
+            0.63,
+            0.55
+            ],
+        "hsla_value": [
+            339,
+            0.89,
+            0.2,
+            0.6
+            ]
+    },
+    {
+        "id": 1087,
+        "uid": "e4395977-59af-4f71-962f-9ae36ab462ad",
+        "hex_value": "#6838d1",
+        "color_name": "lemon",
+        "hsl_value": [
+            300,
+            0.97,
+            0.47
+            ],
+        "hsla_value": [
+            169,
+            0.12,
+            0.2,
+            0.8
+            ]
+    },
+    {
+        "id": 9974,
+        "uid": "723cfb01-408f-4854-a0e0-22f32580dd51",
+        "hex_value": "#1cd6ca",
+        "color_name": "taupe",
+        "hsl_value": [
+            31,
+            0.95,
+            0.15
+            ],
+        "hsla_value": [
+            294,
+            0.23,
+            0.13,
+            0.9
+            ]
+    },
+    {
+        "id": 2390,
+        "uid": "995dcd24-dfe0-4f8a-a2de-ac95d804d9ae",
+        "hex_value": "#c97aef",
+        "color_name": "blue",
+        "hsl_value": [
+            18,
+            0.5,
+            0.46
+            ],
+        "hsla_value": [
+            332,
+            0.33,
+            0.89,
+            0.3
+            ]
+    },
+    {
+        "id": 9900,
+        "uid": "5383d13d-95e6-445f-95ae-eeb9242f7856",
+        "hex_value": "#474d2f",
+        "color_name": "teal",
+        "hsl_value": [
+            327,
+            0.13,
+            0.23
+            ],
+        "hsla_value": [
+            58,
+            0.95,
+            0.56,
+            0.5
+            ]
+    },
+    {
+        "id": 6013,
+        "uid": "53d49b95-3963-403f-9992-36cfbec126a5",
+        "hex_value": "#524ec4",
+        "color_name": "cyan",
+        "hsl_value": [
+            272,
+            0.89,
+            0.88
+            ],
+        "hsla_value": [
+            76,
+            0.95,
+            0.49,
+            0.5
+            ]
+    }
+]
---
+++ lectures/week03/4-Thursday/MY-LectureNotes/daily_problem/problems.py
@@ -0,0 +1,25 @@
+
+
+
+#1. Given a list ["Elie", "Tim", "Matt"], return a new list with only the first letter (["E", "T", "M"])
+
+#2. Print out the numbers 1-10 from the list below
+nums = [
+    {"num": 1},
+    {"num": 2},
+    {"num": 3},
+    {"num": 4},
+    {"num": 5},
+    {"num": 6},
+    {"num": 7},
+    {"num": 8},
+    {"num": 9},
+    {"num": 10},
+]
+
+#3. Given two lists ["CA", "NJ", "RI"] and ["California", "New Jersey", "Rhode Island"] return a dictionary that looks like this {'CA': 'California', 'NJ': 'New Jersey', 'RI': 'Rhode Island'}
+
+abbreviations = ["CA", "NJ", "RI"] 
+state_names = ["California", "New Jersey", "Rhode Island"]
+
+#4. see colorsProblems.py
