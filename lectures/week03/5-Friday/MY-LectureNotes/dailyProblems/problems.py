b4e06c6bdd06d759447ad36ffba7836f0477821a
Veronica Lino <Veronica@digitalcrafts.com>
2022-07-22 14:46:01 -0500

    huhyn restaurant

--- lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
+++ lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
@@ -1,6 +1,20 @@
-# Return a new list with each element multiplied by 5
+#1 Return a new list with each element multiplied by 5
 
-let nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
 
 
-# Given a list [("name", "Elie"), ("job", "Instructor")], create a dictionary that looks like this {'job': 'Instructor', 'name': 'Elie'} (the order does not matter).
+#2 Given a list [("name", "Elie"), ("job", "Instructor")], create a dictionary that looks like this {'job': 'Instructor', 'name': 'Elie'} (the order does not matter).
+
+info = [  ("name", "Elie"), ("job", "Instructor")]
+#          0                      1 
+obj = {}
+
+for el in range(len(info)): # 0, 1
+    print(info[el][1])
+    # obj[info[el][0]] = info[el][1]
+    # print(info[el][0])
+    
+print(obj)
+    
+    
+    
