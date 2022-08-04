00dd6b85737e81ad5e30f7f781de01a90fc655c7
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-08-04 12:13:23 -0400

    morning lecture

--- lectures/week05/3-Wednesday/MY-LectureNotes/fetch.js
+++ lectures/week05/3-Wednesday/MY-LectureNotes/fetch.js
@@ -1,5 +1,28 @@
-fetch(url) //XHR
-.then((results)=>results.json())
-.then(data=>{
 
-})
+const ourFetch = (url) => {
+
+    let promise = new Promise((resolve, reject)=>{
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url) //GET, url
+
+        xhr.send()
+
+        xhr.onreadystatechange = ()=>{ //similar to event listener
+
+            if(xhr.readyState == 4 && xhr.status == 200){
+                let data = JSON.parse(xhr.responseText)
+                console.log(data);
+                resolve(data)
+            }
+            else{
+                reject('error in data')
+            }
+        }
+    })
+
+    return promise;
+}
+
+window.ourFetch = ourFetch
